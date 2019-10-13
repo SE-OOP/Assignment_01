b00096904aaa4bfbfa1ac5c27e806914770ba9c0
yhs-123 <56498911+yhs-123@users.noreply.github.com>
2019-10-13 14:33:31 +0800

    Create EX06_50.cpp
    This is my assignment_01
    Student ID ：201826030127

---
+++ EX06_50.cpp
@@ -0,0 +1,12 @@
+// Exercise 6.50 Solution: Ex06_50.cpp
+// Comparing call by value and call by reference.
+#include <iostream>
+using namespace std;
+
+int tripleByValue( int ); // function prototype
+void tripleByReference( int & ); // function prototype
+
+int main()
+{
+   return 0;
+}
