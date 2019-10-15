e2d228b31b2cc07a08d2b945d5f5d589aeb42a48
201816040123 <56589303+201816040123@users.noreply.github.com>
2019-10-15 19:36:13 +0800

    Create Ex06_50.cpp
    This is my assigenment_01
    Student ID: 201816040123

---
+++ Ex06_50.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  //allows program to output date to the screen
+
+using namespace std;  //programs uses std
+
+//function main begins program execution
+int main()
+{
+    int a;//integer to compare
+    cout << "请输入一个整数" << endl;//prompt user for data
+    cin>>a;//read a integer from user
+    cout << "输出" << endl;//prompt user for data
+    cout<<a<<endl;//print read a integer
+    return 0;
+}//end function main
