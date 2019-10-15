39c33fe57288f64d64e11652d3ff2b10009d0f4e
小c <56589303+201816040123@users.noreply.github.com>
2019-10-15 23:31:08 +0800

    Update Ex06_50.cpp

--- Ex06_50.cpp
+++ Ex06_50.cpp
@@ -2,13 +2,24 @@
 
 using namespace std;   //programs uses std
 
+int tripByValue(int x)
+{
+    return x*3;
+}
+
+int tripByReference(int &x)
+{
+    return x*3;
+}
+
 //function main begins program execution
 int main()
 {
-    int a;//integer to compare
+    int count;//integer to compare
     cout << "请输入一个整数" << endl;//prompt user for data
-    cin>>a;//read a integer from user
-    cout << "输出" << endl;//prompt user for data
-    cout<<a<<endl;//print read a integer
+    cin>>count;//read a integer from user
+    cout << "输出(三倍)" << endl;//prompt user for data
+    cout<<"第一种:"<<tripByValue(count)<<endl;//print read a integer
+    cout<<"第二种:"<<tripByReference(count)<<endl;//print read a integer
     return 0;
 }//end function main
