5fcaa7a243aa2fee2bbbcc495487e1007bb2c868
今夕何夕 <53158211+LXGTLHL@users.noreply.github.com>
2019-10-16 17:25:30 +0800

    Update Ex06_50.cpp

--- 201816040220/Ex06_50.cpp
+++ 201816040220/Ex06_50.cpp
@@ -6,23 +6,27 @@ int tripleByValue(int);
 void tripleByReference(int &);
 int main()
 {
-    int count=1;
+    int count;
+    cin>>count;                                                                   
 
-    tripleByValue(count);
-    cout << tripleByValue(count)<<endl;
+    cout <<"Value of count before call to tripleByValue() is:"<<count<<endl;      //调用前count值
+    tripleByValue(count);                                                          //调用tripleByValue函数
+    cout <<"Value returned from tripleByValue() is:"<<tripleByValue(count)<<endl;   //调用后count副本值
+    cout <<"Value of count (in main) after tripleCallByValue() is: "<<count<<endl;   //调用后count值
 
-    tripleByReference(count);
-    cout << count<< endl;
+    cout <<"Value of count before call to tripleByReference() is: "<<count<<endl;     //调用前count值
+    tripleByReference(count);                                                          //调用tripleByReference函数
+    cout <<"Value of count (in main) after call to tripleByReference() is:" <<count<< endl; //调用后count值
     return 0;
 }
 
-int tripleByValue(int x)
+int tripleByValue(int x)                                                //按值传递count的副本
 {
     x=x*3;
     return x;
 }
 
-void tripleByReference(int &y)
+void tripleByReference(int &y)                                           //引用参数按引用传递计数
 {
     y=y*3;
 }
