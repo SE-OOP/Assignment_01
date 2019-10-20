b207e901bfec8d3facc17086d4d274e19a022d91
201816040314 <56504172+201816040314@users.noreply.github.com>
2019-10-20 18:55:39 +0800

    Update Ex06_50.cpp
    This is my assignment_01
    Student ID:201816040314

--- 201816040314/Ex06_50.cpp
+++ 201816040314/Ex06_50.cpp
@@ -10,10 +10,15 @@ void tripleByReference(int & num){
 
 int main()
 {
-    int count=10;  //count的值为10
-    cout<<"tripleByValue的结果:"<<tripleByValue(count)<<endl;  //用函数tripleByValue求count的3倍值
-    cout<<"tripleByReference的结果:";
-    tripleByReference(count);             //用函数tripleByReference求count的3倍值
+    int count=10;
+    cout<<"请输入一个整数count:";
+    cin>>count;
+    cout<<"调用tripleByValue()函数之前count的值为:"<<count<<endl;
+    cout<<"调用tripleByValue()函数返回的值为: "<<tripleByValue(count)<<endl;
+    cout<<"调用tripleByValue()函数之后count的值为:"<<count<<endl<<endl;
+    cout<<"调用tripleByReference()函数之前的count值为:"<<count<<endl;
+    cout<<"调用tripleByReference()函数之后的count值为:";
+    tripleByReference(count);
     cout<<count<<endl;
 
 }
