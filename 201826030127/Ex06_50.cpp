618ed48ef039878292349e52dfaba4414acbd25e
yhs-123 <56498911+yhs-123@users.noreply.github.com>
2019-10-21 07:55:53 +0800

    Create Ex06_50.cpp
    This is my assignment_01.
    Student ID:201826030127

---
+++ 201826030127/Ex06_50.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int tripleByValue(int); //function prototype(Value pass)
+void tripleByReference(int &);//function prototype(reference pass)
+
+int main()
+{
+    int count;
+
+    cin >> count;
+    cout << "count=" << count << " before tripleByValue\n" << endl;
+    cout << "Value returned by tripleByValue:" << tripleByValue(count) << endl;
+    cout << "count=" << count << " after tripleByValue" << endl;
+
+
+    cout << "count=" << count << " before tripleByReference" << endl;
+    tripleByReference(count);
+    cout << "after tripleByReference:" << count << endl;
+}
+
+
+int tripleByValue(int number)
+{
+    return 3*number;
+}//end function tripleByValue
+
+
+void tripleByReference(int &numberRef)
+{
+    numberRef = 3 * numberRef;
+}//end function tripleByReference
+
+
+
