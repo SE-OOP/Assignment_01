0c42fa5e4b4a6305348472073b5884933564b71a
huatzy <56495508+huatzy@users.noreply.github.com>
2019-10-20 10:44:00 +0800

    Update Ex06_50.cpp

--- 201816040201/Ex06_50.cpp
+++ 201816040201/Ex06_50.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 
 using namespace std;
-int tripleByValue(int x)
+int tripleByValue(int x)//传值
 {
     return x*=3;
 }
-void tripleReference(int &cnt)
+void tripleReference(int &cnt)//引用
 {
      cnt*=3;
 }
 int main()
 {
   int cnt;
-  cin>>cnt;
+  cin>>cnt;//输入
   int &ocnt=cnt;
-  cout<<"cnt:"<<cnt<<" "<<"tripleByValue:"<<  tripleByValue(cnt)<<endl;
+  cout<<"cnt:"<<cnt<<" "<<"tripleByValue:"<<  tripleByValue(cnt)<<endl;//传值后两个数的大小
 
      tripleReference(cnt);
-   cout<<"tripleReference:"<<cnt <<" "<<"cnt:"<<cnt<<endl;
+   cout<<"tripleReference:"<<cnt <<" "<<"cnt:"<<cnt<<endl;//引用后两个值的大小
 
     return 0;
 }
