9e0c37a170ea30b6535099c286f4976012b975c3
zhangyao1999 <56502471+zhangyao1999@users.noreply.github.com>
2019-10-20 23:08:53 +0800

    Update Ex06_50.cpp
    id 201816040324

--- 201816040324/Ex06_50.cpp
+++ 201816040324/Ex06_50.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+int tripleByVlaue (int) ;
+void tripleByReference (int &) ;
+
+int main()
+{
+    int count ;
+
+    cout <<"Enter a value for count: ";
+    cin >>count ;
+    cout <<"Value returned from tripleByValue() is: "<<count<<"\n" ;
+    cout <<"Value returned from tripleByValue() is: "<<tripleByVlaue(count)<<endl ;
+    cout <<"Value of count (in main) after tripleCallByValue() is: "<<count<<endl ;
+    cout <<"Value of count before call to tripleByReference() is: "<<count<<"\n" ;
+    tripleByReference (count) ;
+    cout <<"Value of count (in main) after call to tripleByReference() is: "<<count<<endl ;
+    return 0;
+}
+int tripleByVlaue (int number)
+{
+     return number =3*number ;
+}
+void tripleByReference (int &numberref)
+{
+    numberref =3*numberref ;
+}
