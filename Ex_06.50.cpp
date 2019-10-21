c792f583986de2752d8e84f5e1ebe0f81277da21
201816040301 <56689696+201816040301@users.noreply.github.com>
2019-10-21 18:47:22 +0800

    Rename Ex06.50.cpp to Ex_06.50.cpp
    This is my assignment_01
    Sudent ID : 201816040301

---
+++ Ex_06.50.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+int tripleByValue(int);//function prototype
+
+void tripleByReference(int &);//function prototype
+
+
+
+int main()
+
+{
+
+    int a,b;//定义参数
+
+    //对tripleByValue函数
+
+    cout <<"Enter a value for count:";
+
+    cin >> a;
+
+    cin >> b;
+
+    cout <<"Value of count before call to tripleByValue() is:"<<a<<endl;
+
+    cout <<"Value returned from tripleByValue() is:"<<tripleByValue(a)<<endl;
+
+    //cout <<"Value of count (in main) after tripleCallByValue() is:"<<count<<endl;
+
+
+
+    //对tripleByReferences参数
+
+    cout <<"Value of count before call to tripleByReference() is:"<<b<<endl;
+
+    tripleByReference(b);
+
+    cout <<"Value of count (in main) after call to tripleByReference() is:"<<b<<endl;
+
+    return 0;
+
+}
+
+//按值传递
+
+int tripleByValue(int a)
+
+{
+
+  return a=a*a*a;
+
+
+
+}
+
+//按引用传递
+
+void  tripleByReference(int &b)
+
+{
+
+    b=b*b*b;
+
+}
