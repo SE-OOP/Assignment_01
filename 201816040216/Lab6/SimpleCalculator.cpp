ef09e50a37aa6dee9f21c101b8d88ce8ca735f8b
xinbashizi <56757060+xinbashizi@users.noreply.github.com>
2019-11-21 17:57:27 +0800

    Create SimpleCalculator.cpp

---
+++ 201816040216/Lab6/SimpleCalculator.cpp
@@ -0,0 +1,29 @@
+// Lab Exercise 6: SimpleCalculator.cpp
+
+#include "SimpleCalculator.h"
+
+/* Write definition for add member function */
+double SimpleCalculator::add( double a,double b ) const
+{
+    return a + b;
+}//end function add
+
+// function subtract definition
+double SimpleCalculator::subtract( double a, double b ) const
+{
+   return a - b;
+} // end function subtract
+
+// function multiply definition
+double SimpleCalculator::multiply( double a, double b ) const
+{
+   return a * b;
+} // end function multiply
+
+/* Write definition for divide member function */
+double SimpleCalculator::divide( double a,double b )const
+{
+    return a / b;
+}//end function divide
+
+
